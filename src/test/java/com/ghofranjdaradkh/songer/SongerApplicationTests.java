9dec93cad081a70ff7322ec5c18de0ac9253dc26
ghofranjdaradkh <ghofran_daradkh@hotmail.com>
2023-09-04 20:51:17 +0300

    lab11

--- src/test/java/com/ghofranjdaradkh/songer/SongerApplicationTests.java
+++ src/test/java/com/ghofranjdaradkh/songer/SongerApplicationTests.java
@@ -3,6 +3,8 @@ package com.ghofranjdaradkh.songer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SongerApplicationTests {
 
@@ -10,4 +12,38 @@ class SongerApplicationTests {
 	void contextLoads() {
 	}
 
+@Test
+	void testConstructor(){
+	Album  Album = new Album("Album1", "Name", 1, 300, "album.jpg");
+	assertEquals("Album1", Album.getTitle());
+	assertEquals("Name", Album.getArtist());
+	assertEquals(1, Album.getSongCount());
+	assertEquals(300, Album.getLength());
+	assertEquals("album.jpg", Album.getImageUrl());
+
+}
+
+	@Test
+	public void testGettersAndSetters() {
+		Album  album = new Album("Album1", "Name", 1, 300, "album.jpg");
+
+
+		album.setTitle("New Title");
+		assertEquals("New Title", album.getTitle());
+
+		album.setArtist("New Artist");
+		assertEquals("New Artist", album.getArtist());
+
+
+		album.setSongCount(20);
+		assertEquals(20, album.getSongCount());
+
+		// Test lengthSeconds
+		album.setLength(480);
+		assertEquals(480, album.getLength());
+
+		// Test imageUrl
+		album.setImageUrl("newImage.jpg");
+		assertEquals("newImage.jpg", album.getImageUrl());
+	}
 }
