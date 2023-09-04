49e7e3be953593516f78e8976b6a517437f1c654
ghofranjdaradkh <ghofran_daradkh@hotmail.com>
2023-09-04 08:51:35 +0300

    initial commit from Spring init

---
+++ src/main/java/com/ghofranjdaradkh/songer/SongerApplication.java
@@ -0,0 +1,13 @@
+package com.ghofranjdaradkh.songer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongerApplication.class, args);
+	}
+
+}
